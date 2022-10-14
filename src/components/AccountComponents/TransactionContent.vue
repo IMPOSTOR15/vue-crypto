761f72d40608d6521dbfc6e2bcd3fccc9d1ba9c1
Andrey Dagaev <dandr212@gmail.com>
2022-10-14 21:21:04 +0300

    edit transaction content

--- src/components/AccountComponents/TransactionContent.vue
+++ src/components/AccountComponents/TransactionContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <li class="transaction-wrapper">
+  <!-- <li class="transaction-wrapper">
     <div class="transaction">
       <div class="transcation-id">#{{ transaction.transactionContent.id }}</div>
       <div class="transcation-id">
@@ -17,6 +17,29 @@
         </div>
       </div>
     </div>
+  </li> -->
+  <li class="transaction-wrapper">
+    <div class="transaction-container">
+      <div class="transaction-half">
+        <div class="transaction-info">
+          <div class="arrow-sapce">
+            <img v-if="transaction.transactionContent.type === 'Output'" class="transaction-arrow" src="@\assets\arrows\arrow_bottom.svg" alt="">
+            <img v-if="transaction.transactionContent.type === 'Input'" class="transaction-arrow" src="@\assets\arrows\arrow_top.svg" alt="">
+          </div>
+          <div class="transaction-content">
+            <div class="transaction-text">Curency: {{ transaction.transactionContent.curency }}</div>
+            <div class="transaction-value">
+              <p>Amount: {{ transaction.transactionContent.value }}</p>
+            </div>
+          </div>
+        </div>
+        <button class="button" @click="showMore = !showMore">Show more</button>
+      </div>
+      <div class="transaction-full" v-if="showMore">
+        <p>Transaction time: {{ transaction.transactionContent.date }}</p>
+        <p>Transaction id: {{ transaction.transactionContent.id }}</p>
+      </div>
+    </div>
   </li>
 </template>
 
@@ -26,6 +49,11 @@ export default {
     transaction: {
       type: Object,
       reqired: true
+    }
+  },
+  data() {
+    return {
+      showMore: false,
     }
   }
 }
@@ -54,5 +82,24 @@ export default {
 .transaction-arrow {
   width: 40px;
   rotate: 180deg;
+}
+
+.transaction-half {
+  display: flex;
+  flex-direction: row;
+  padding: 20px 20px 0px 20px;
+}
+
+.button {
+  margin-top: -5px;
+  margin-bottom: 10px;
+  /* align-self: flex-end; */
+}
+.transaction-half {
+  justify-content: space-between;
+}
+.transaction-info {
+  display: flex;
+  flex-direction: column;
 }
 </style>
